27aa3931143c17a82841a7ff0273da42f9f2ea95
SonitBahl <sonitbahl@gmail.com>
2024-03-18 03:11:10 +0530

    updated front-end

--- static/script.js
+++ static/script.js
@@ -6,10 +6,13 @@ let hill5 = document.getElementById('hill5');
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
+    let sectionHeight = document.querySelector('.parallax').offsetHeight;
+    let maxScrollValue = sectionHeight * 1.5;
+    let adjustedScrollValue = value % maxScrollValue;
 
-    text.style.marginTop = value * 2.5 + 'px';
-    leaf.style.top = value * -0.5 + 'px';
-    hill5.style.left = value * 0.5 + 'px';
-    hill4.style.left = value * -0.5 + 'px';
-    hill1.style.top = value * 0.25 + 'px';
+    text.style.marginTop = adjustedScrollValue * 2.5 + 'px';
+    leaf.style.top = adjustedScrollValue * -0.5 + 'px';
+    hill5.style.left = adjustedScrollValue * 0.5 + 'px';
+    hill4.style.left = adjustedScrollValue * -0.5 + 'px';
+    hill1.style.top = adjustedScrollValue * 0.25 + 'px';
 });
